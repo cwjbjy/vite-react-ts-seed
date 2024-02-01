540d7fb50c8e0e663f22ad643bf1d296581f6ee9
cwjbjy <595842570@qq.com>
2024-02-01 09:01:13 +0800

    feat: 修改优先级

--- config/env.js
+++ config/env.js
@@ -7,8 +7,8 @@ const { _, mode } = minimist(process.argv.slice(2));
 const platform = _.filter((o) => o.includes('platform'))[0].split('=')[1];
 
 /* 根据打包配置设置环境变量，先设置的优先级最高 */
-/* 优先级：.env.local > .env.platform.mode > .env.platform  > .env */
-dotenv.config({ path: process.cwd() + '/env/' + '.env.local', override: true });
+/* 优先级：.env.platform.mode > .env.platform >  .env.local > .env */
 dotenv.config({ path: process.cwd() + '/env/' + '.env.' + platform + '.' + mode, override: true });
 dotenv.config({ path: process.cwd() + '/env/' + '.env.' + platform, override: true });
+dotenv.config({ path: process.cwd() + '/env/' + '.env.local', override: true });
 dotenv.config({ path: process.cwd() + '/env/' + '.env', override: true });
