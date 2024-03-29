295809358ceb30f3b38e4f7fb6f63cab1bf06ec6
cwjbjy <595842570@qq.com>
2024-03-29 17:42:57 +0800

    fix: 修复错误

--- config/env.js
+++ config/env.js
@@ -4,11 +4,11 @@ import minimist from 'minimist';
 const { _, mode } = minimist(process.argv.slice(2));
 
 //从自定义启动参数中，提取出platform
-const platform = _.filter((o) => o.includes('platform'))[0].split('=')[1];
+const platform = _.filter((o) => o.includes('platform'))[0]?.split('=')[1] || 'wc1';
 
 /* 根据打包配置设置环境变量，先设置的优先级最高 */
-/* 优先级：.env.platform.mode > .env.platform >  .env.local > .env */
-dotenv.config({ path: process.cwd() + '/env/' + '.env.' + platform + '.' + mode, override: true });
-dotenv.config({ path: process.cwd() + '/env/' + '.env.' + platform, override: true });
-dotenv.config({ path: process.cwd() + '/env/' + '.env.local', override: true });
-dotenv.config({ path: process.cwd() + '/env/' + '.env', override: true });
+/* 优先级： .env.local > .env.platform.mode > .env.platform > .env */
+dotenv.config({ path: process.cwd() + '/env/' + '.env.local' });
+dotenv.config({ path: process.cwd() + '/env/' + '.env.' + platform + '.' + mode });
+dotenv.config({ path: process.cwd() + '/env/' + '.env.' + platform });
+dotenv.config({ path: process.cwd() + '/env/' + '.env' });
